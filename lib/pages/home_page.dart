b0fbdbacbb618e7b93b9951546c1458a5cef2466
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-28 11:43:31 +0300

    Create home_page.dart

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:nurse_app/components/second_button.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.location_on),
+                        onPressed: () {},
+                      ),
+                      const Text(
+                        'Beirut, Lebanon',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.notifications_none,
+                      color: Color(0xFF7BB442),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.person),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Welcome Back User',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: const DecorationImage(
+                          image: AssetImage('assets/images/image1.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        child: const Center(
+                          child: Text(
+                            'Request your Nurse Now',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 36,
+                              fontWeight: FontWeight.w700,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              MySecondButton(
+                onTap: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                buttonText: 'Immediate Request',
+              ),
+              const SizedBox(height: 15),
+              MySecondButton(
+                onTap: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                buttonText: 'Make an Appointment',
+              ),
+              const SizedBox(height: 50),
+              const Center(
+                child: Image(
+                  image: AssetImage('assets/images/logo.png'),
+                  height: 150,
+                  width: 300,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
