b9fc6ca110a73e31a1259e68a2f373a9dded9883
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-28 11:43:23 +0300

    Create second_button.dart

---
+++ lib/components/second_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MySecondButton extends StatelessWidget {
+  final Function()? onTap;
+  final String buttonText;
+
+  const MySecondButton({super.key, required this.onTap, required this.buttonText});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: 50,
+        margin: const EdgeInsets.symmetric(horizontal: 60),
+        decoration: BoxDecoration(
+          color: const Color(0xFF7BB442),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
