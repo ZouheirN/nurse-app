05787dd136e2a6aedcce9b9a41d4002d2a6d1af9
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-28 11:43:14 +0300

    Create navbar.dart

---
+++ lib/pages/navbar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:nurse_app/pages/home_page.dart';
+
+class Navbar extends StatefulWidget {
+  const Navbar({super.key});
+
+  @override
+  State<Navbar> createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  int myIndex = 0;
+  List<Widget> widgetList = const [
+    HomePage(),
+    HomePage(),
+    HomePage(),
+    HomePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: widgetList[myIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        backgroundColor: Colors.white,
+        onTap: (index) {
+          setState(() {
+            myIndex = index;
+          });
+        },
+        currentIndex: myIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Image(
+                image: AssetImage('assets/images/home.png'),
+                height: 30,
+                width: 30),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+                image: AssetImage('assets/images/history.png'),
+                height: 30,
+                width: 30),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+                image: AssetImage('assets/images/info.png'),
+                height: 30,
+                width: 30),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Image(
+                image: AssetImage('assets/images/profile.png'),
+                height: 30,
+                width: 30),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
