a74b260a91cb10129a24b61e1e2ae2b7ceaf4502
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-29 23:34:46 +0300

    Create labeled_date.dart

---
+++ lib/components/labeled_date.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class LabeledDateField extends StatefulWidget {
+  final String label;
+  final DateTime currentDate;
+
+  const LabeledDateField({
+    super.key,
+    required this.label,
+    required this.currentDate,
+  });
+
+  @override
+  _LabeledDateFieldState createState() => _LabeledDateFieldState();
+}
+
+class _LabeledDateFieldState extends State<LabeledDateField> {
+  late bool _isFocused;
+  DateTime? _selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFocused = false;
+  }
+
+  void _onFocusChange(bool hasFocus) {
+    setState(() {
+      _isFocused = hasFocus;
+    });
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: widget.currentDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2100),
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 65,
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.label,
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 3),
+          Expanded(
+            child: Focus(
+              onFocusChange: _onFocusChange,
+              child: GestureDetector(
+                onTap: () => _selectDate(context),
+                child: AbsorbPointer(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: _selectedDate == null
+                          ? 'dd/mm/yyyy'
+                          : "${_selectedDate!.day}/${_selectedDate!.month}/${_selectedDate!.year}",
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFFE7E7E7)),
+                      ),
+                      fillColor: _isFocused
+                          ? const Color.fromARGB(255, 245, 245, 245)
+                          : const Color(0xFFE7E7E7),
+                      filled: true,
+                      contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                    ),
+                    style: TextStyle(
+                      color: _selectedDate != null ? Colors.black : Colors.grey,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
