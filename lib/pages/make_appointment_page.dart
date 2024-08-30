822481b2a4515728241f8c4e9c6357d0c990140d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-30 11:28:36 +0300

    Update make_appointment_page.dart

--- lib/pages/make_appointment_page.dart
+++ lib/pages/make_appointment_page.dart
@@ -155,7 +155,7 @@ class MakeAppointmentPage extends StatelessWidget {
               const SizedBox(height: 20),
               MyThirdButton(
                 onTap: () {
-                  Navigator.pushNamed(context, '/home');
+                  Navigator.pushNamed(context, '/pendingPage');
                 },
                 buttonText: 'Submit',
               ),
