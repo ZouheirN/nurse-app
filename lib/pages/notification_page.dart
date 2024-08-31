d98f3930e32cb83d7b58f3070e0402fc5bdd7511
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-08-31 16:20:16 +0300

    Create notification_page.dart

---
+++ lib/pages/notification_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:nurse_app/components/notification_card.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.location_on),
+                        onPressed: () {},
+                      ),
+                      const Text(
+                        'Beirut, Lebanon',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      Icons.notifications_none,
+                      color: Color(0xFF7BB442),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    icon: const Icon(Icons.person),
+                    onPressed: () {},
+                  ),
+                  const Text(
+                    'Welcome Back User',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              const Center(
+                child: NotificationCard(
+                  title: 'Your request was accepted',
+                  description: 'Check out the details',
+                  time: '12:00',
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: NotificationCard(
+                  title: 'Your request was accepted',
+                  description: 'Check out the details',
+                  time: '12:00',
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: NotificationCard(
+                  title: 'Your request was accepted',
+                  description: 'Check out the details',
+                  time: '12:00',
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Center(
+                child: NotificationCard(
+                  title: 'Your request was accepted',
+                  description: 'Check out the details',
+                  time: '12:00',
+                ),
+              ),
+              const SizedBox(height: 30),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
