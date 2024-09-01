bb4a3df721c7d9b4e07c258e277368b79f4bf24e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-01 18:01:34 +0300

    Connect signup to api

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,9 +1,82 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:nurse_app/components/button.dart';
 import 'package:nurse_app/components/textfield.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nurse_app/consts.dart';
 
 class SignupPage extends StatelessWidget {
-  const SignupPage({super.key});
+  SignupPage({super.key});
+
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordConfirmationController = TextEditingController();
+
+  void signup(String name, phoneNumber, email, password, passwordConfirmation,
+      BuildContext context) async {
+    try {
+      final response = await http.post(
+        Uri.parse('$HOST/register'),
+        body: {
+          'name': name,
+          'phone_number': phoneNumber,
+          'email': email,
+          'password': password,
+          'password_confirmation': passwordConfirmation,
+        },
+      );
+      if (response.statusCode == 201) {
+        final jsonData = json.decode(response.body);
+        final token = jsonData['token'];
+
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString(KEY_ACCESS_TOKEN, token);
+
+        Navigator.pushNamed(context, '/home');
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Sign Up Failed'),
+              content: const Text('Failed to sign up. Please try again.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      print(e.toString());
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('An error occurred. Please try again later.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,33 +117,52 @@ class SignupPage extends StatelessWidget {
                 ),
               ),
               const SizedBox(height: 15),
-              const MyTextField(
-                icon: Icon(Icons.person_outlined),
+              MyTextField(
+                controller: nameController,
+                icon: const Icon(Icons.person_outlined),
                 hintText: 'Username',
                 obscureText: false,
               ),
               const SizedBox(height: 14),
-              const MyTextField(
-                icon: Icon(Icons.phone_outlined),
+              MyTextField(
+                controller: phoneNumberController,
+                icon: const Icon(Icons.phone_outlined),
                 hintText: 'Phone Number',
                 obscureText: false,
               ),
               const SizedBox(height: 14),
-              const MyTextField(
-                icon: Icon(Icons.mail_outline),
+              MyTextField(
+                controller: emailController,
+                icon: const Icon(Icons.mail_outline),
                 hintText: 'Email',
                 obscureText: false,
               ),
               const SizedBox(height: 14),
-              const MyTextField(
-                icon: Icon(Icons.lock_outline),
+              MyTextField(
+                controller: passwordController,
+                icon: const Icon(Icons.lock_outline),
                 hintText: 'Password',
+                obscureText: true,
+              ),
+              const SizedBox(height: 14),
+              MyTextField(
+                controller: passwordConfirmationController,
+                icon: const Icon(Icons.lock_outline),
+                hintText: 'Confirm Password',
                 obscureText: true,
               ),
               const SizedBox(height: 12),
               MyButton(
-                onTap: () {},
-                buttonText: 'Signup',
+                onTap: () {
+                  signup(
+                      nameController.text.toString(),
+                      phoneNumberController.text.toString(),
+                      emailController.text.toString(),
+                      passwordController.text.toString(),
+                      passwordConfirmationController.text.toString(),
+                      context);
+                },
+                buttonText: 'Sign Up',
               ),
               const SizedBox(height: 10),
               Row(
