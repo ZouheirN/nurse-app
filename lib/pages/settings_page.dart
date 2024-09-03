c34b3096379859b78a630f55fbb95d7a121e836e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-03 19:18:05 +0300

    Update settings_page.dart

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nurse_app/components/logout_button.dart';
 import 'package:nurse_app/components/settings_button.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -8,38 +9,53 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      body: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const SizedBox(height: 20),
-              const Center(
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  height: 110,
-                  width: 220,
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 40),
+                    const Center(
+                      child: Image(
+                        image: AssetImage('assets/images/logo.png'),
+                        height: 110,
+                        width: 220,
+                      ),
+                    ),
+                    const SizedBox(height: 40),
+                    Center(
+                      child: SettingsButton(
+                        icon: const Icon(Icons.account_circle),
+                        buttonText: 'Account Details',
+                        onTap: () {},
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Center(
+                      child: SettingsButton(
+                        icon: const Icon(Icons.location_on),
+                        buttonText: 'Update Location',
+                        onTap: () {},
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    LogoutButton(
+                      icon: const Icon(Icons.logout),
+                      buttonText: 'Logout',
+                      onTap: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 40),
-              Center(
-                child: SettingsButton(
-                  icon: const Icon(Icons.account_circle),
-                  buttonText: 'Account Details',
-                  onTap: () {},
-                ),
-              ),
-              const SizedBox(height: 20),
-              Center(
-                child: SettingsButton(
-                  icon: const Icon(Icons.location_on),
-                  buttonText: 'Update Location',
-                  onTap: () {},
-                ),
-              ),
-              const SizedBox(height: 40),
+              // The "Powered By" section at the bottom
               Padding(
-                padding: const EdgeInsets.only(bottom: 30.0),
+                padding: const EdgeInsets.only(bottom: 30),
                 child: Center(
                   child: Column(
                     children: [
@@ -59,7 +75,7 @@ class SettingsPage extends StatelessWidget {
               ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
