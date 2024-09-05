3bf9630a8a638d13a848db16017a06cf36dd45a0
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-06 00:06:31 +0300

    Create add_service_page.dart

---
+++ lib/pages/admin/add_service_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:nurse_app/components/third_button.dart';
+import 'package:nurse_app/components/labeled_textfield_admin.dart';
+import 'package:nurse_app/components/labeled_mini_textfield_admin.dart';
+
+class AddServicePage extends StatelessWidget {
+  const AddServicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        leadingWidth: 75,
+        leading: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Center(
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ),
+        title: const Text(
+          'Add New Nurse',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(height: 40),
+                  const LabeledTextfieldAdmin(
+                    label: 'Service Name',
+                    keyboardType: TextInputType.name,
+                  ),
+                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 35),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: LabeledMiniTextfieldAdmin(
+                            label: 'Service Price',
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Expanded(
+                          child: LabeledMiniTextfieldAdmin(
+                            label: 'Sale Price',
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  MyThirdButton(
+                    onTap: () {},
+                    buttonText: 'Add Service',
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
