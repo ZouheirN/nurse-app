7550b5d0efc302c0c9c1cdbd0570e5ce7acad9b3
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-06 00:06:14 +0300

    Create labeled_mini_textfield_admin.dart

---
+++ lib/components/labeled_mini_textfield_admin.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+class LabeledMiniTextfieldAdmin extends StatefulWidget {
+  final String label;
+  final TextInputType keyboardType;
+
+  const LabeledMiniTextfieldAdmin({
+    super.key,
+    required this.label,
+    this.keyboardType = TextInputType.text,
+  });
+
+  @override
+  _LabeledMiniTextfieldAdminState createState() =>
+      _LabeledMiniTextfieldAdminState();
+}
+
+class _LabeledMiniTextfieldAdminState extends State<LabeledMiniTextfieldAdmin> {
+  late bool _isFocused;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFocused = false;
+  }
+
+  void _onFocusChange(bool hasFocus) {
+    setState(() {
+      _isFocused = hasFocus;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      padding: const EdgeInsets.symmetric(horizontal: 5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.label,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 3),
+          Expanded(
+            child: Focus(
+              onFocusChange: _onFocusChange,
+              child: TextField(
+                keyboardType: widget.keyboardType,
+                decoration: InputDecoration(
+                  enabledBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Color(0xFF7BB442)),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(color: Color(0xFF7BB442)),
+                  ),
+                  fillColor: _isFocused
+                      ? const Color(0xFFE8FFD1)
+                      : const Color(0xFFE8FFD1),
+                  filled: true,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
