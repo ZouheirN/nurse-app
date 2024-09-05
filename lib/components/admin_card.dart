f610f62cfeb86ebca39639bb19d9eff40c6fd896
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-05 12:35:55 +0300

    Create admin_card.dart

---
+++ lib/components/admin_card.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AdminCard extends StatelessWidget {
+  final String imagePath;
+  final String text;
+  final VoidCallback onTap;
+
+  const AdminCard({
+    super.key,
+    required this.imagePath,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 330,
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: const Color.fromARGB(255, 221, 221, 221),
+            width: 1,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              imagePath,
+              width: 50,
+              height: 50,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
