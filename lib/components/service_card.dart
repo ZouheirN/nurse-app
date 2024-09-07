43cba2f4bb15dfd8b24708b05052ddd67ec3ac1d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-07 23:41:32 +0300

    Update service_card.dart

--- lib/components/service_card.dart
+++ lib/components/service_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ServiceCard extends StatelessWidget {
+class ServiceCard extends StatefulWidget {
   final String imagePath;
   final String title;
   final int price;
@@ -15,74 +15,98 @@ class ServiceCard extends StatelessWidget {
   });
 
   @override
+  _ServiceCardState createState() => _ServiceCardState();
+}
+
+class _ServiceCardState extends State<ServiceCard> {
+  bool isSelected = false;
+
+  void _toggleSelection() {
+    setState(() {
+      isSelected = !isSelected;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 85,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 4,
-            spreadRadius: 2,
+    return GestureDetector(
+      onTap: _toggleSelection,
+      child: Container(
+        width: 85,
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(
+            color: isSelected ? Colors.green : Colors.transparent,
+            width: 2,
           ),
-        ],
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            imagePath,
-            height: 60,
-            width: 85,
-          ),
-          const SizedBox(height: 8),
-          Text(
-            title,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.w500,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 4,
+              spreadRadius: 2,
             ),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 8),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: Alignment.center,
-                children: [
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              widget.imagePath,
+              height: 60,
+              width: 85,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: (widget.salePrice != null)
+                  ? MainAxisAlignment.spaceBetween
+                  : MainAxisAlignment.center,
+              children: [
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Text(
+                      '${widget.price}\$',
+                      style: TextStyle(
+                        color: widget.salePrice != null
+                            ? Colors.grey
+                            : Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    if (widget.salePrice != null)
+                      Positioned.fill(
+                        child: CustomPaint(
+                          painter: DiagonalLinePainter(),
+                        ),
+                      ),
+                  ],
+                ),
+                if (widget.salePrice != null) ...[
+                  const SizedBox(width: 5),
                   Text(
-                    '$price\$',
-                    style: TextStyle(
-                      color: salePrice != null ? Colors.grey : Colors.black,
+                    '${widget.salePrice}\$',
+                    style: const TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.w700,
                     ),
                   ),
-                  if (salePrice != null)
-                    Positioned.fill(
-                      child: CustomPaint(
-                        painter: DiagonalLinePainter(),
-                      ),
-                    ),
                 ],
-              ),
-              if (salePrice != null) ...[
-                const SizedBox(width: 5),
-                Text(
-                  '$salePrice\$',
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
               ],
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
