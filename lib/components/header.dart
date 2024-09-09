e1c599ee3efbbd7b3571e527776985367a943c6d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-09 22:39:33 +0300

    Update header.dart

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,8 +1,9 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:nurse_app/consts.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:nurse_app/consts.dart';
 
 class Header extends StatefulWidget {
   const Header({super.key});
@@ -13,14 +14,15 @@ class Header extends StatefulWidget {
 
 class _HeaderState extends State<Header> {
   String name = '';
+  String location = 'Loading...';
 
   @override
   void initState() {
     super.initState();
-    fetchUserName();
+    fetchUserData();
   }
 
-  Future<void> fetchUserName() async {
+  Future<void> fetchUserData() async {
     try {
       final prefs = await SharedPreferences.getInstance();
       final token = prefs.getString(KEY_ACCESS_TOKEN);
@@ -38,10 +40,46 @@ class _HeaderState extends State<Header> {
           setState(() {
             name = jsonData['name'].split(' ')[0] ?? '';
           });
+
+          // Fetch location based on latitude and longitude
+          final double? latitude = jsonData['latitude'];
+          final double? longitude = jsonData['longitude'];
+
+          if (latitude != null && longitude != null) {
+            fetchLocationFromCoordinates(latitude, longitude);
+          } else {
+            setState(() {
+              location = 'Location not available';
+            });
+          }
         }
       }
     } catch (e) {
       print('Error fetching user data: $e');
+    }
+  }
+
+  Future<void> fetchLocationFromCoordinates(double latitude, double longitude) async {
+    try {
+      final response = await http.get(
+        Uri.parse('https://nominatim.openstreetmap.org/reverse?format=json&lat=$latitude&lon=$longitude'),
+      );
+
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        setState(() {
+          location = jsonData['display_name'] ?? 'Unknown Location';
+        });
+      } else {
+        setState(() {
+          location = 'Unable to fetch location';
+        });
+      }
+    } catch (e) {
+      print('Error fetching location: $e');
+      setState(() {
+        location = 'Unable to fetch location';
+      });
     }
   }
 
@@ -60,9 +98,9 @@ class _HeaderState extends State<Header> {
                     Navigator.pushNamed(context, '/adminDashboard');
                   },
                 ),
-                const Text(
-                  'Beirut, Lebanon',
-                  style: TextStyle(
+                Text(
+                  location,
+                  style: const TextStyle(
                     fontWeight: FontWeight.w700,
                     fontSize: 16,
                   ),
