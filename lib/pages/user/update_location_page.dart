80789ea16d04c11289d0fa8950c85367b2264f4b
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-09 22:41:41 +0300

    Update update_location_page.dart

--- lib/pages/user/update_location_page.dart
+++ lib/pages/user/update_location_page.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:nurse_app/components/third_button.dart';
 import 'package:nurse_app/components/labeled_edit_textfield.dart';
 
-class UpdateLocationPage extends StatelessWidget {
+class UpdateLocationPage extends StatefulWidget {
   const UpdateLocationPage({super.key});
+
+  @override
+  _UpdateLocationPageState createState() => _UpdateLocationPageState();
+}
+
+class _UpdateLocationPageState extends State<UpdateLocationPage> {
+  LatLng _initialPosition = const LatLng(0.0, 0.0);
+  final MapController _mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _setInitialLocation();
+  }
+
+  void _setInitialLocation() async {
+    var currentLocation = const LatLng(33.851338, 35.518375);
+    setState(() {
+      _initialPosition = currentLocation;
+    });
+    _mapController.move(_initialPosition, 15.0);
+  }
+
+  void _updateLocation(LatLng latLng) {
+    setState(() {
+      _initialPosition = latLng;
+    });
+    // Logic to update the location in your backend
+    print("Location Updated: $_initialPosition");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,24 +73,57 @@ class UpdateLocationPage extends StatelessWidget {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(children: [
-          const SizedBox(height: 10),
-          Image.asset(
-            'assets/images/logo.png',
-            height: 300,
-            width: 300,
-          ),
-          const SizedBox(height: 20),
-          const LabeledEditTextfield(
-            label: 'Enter Your Location',
-            keyboardType: TextInputType.text,
-          ),
-          const SizedBox(height: 20),
-          MyThirdButton(
-            onTap: () {},
-            buttonText: 'Save',
-          ),
-        ]),
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            SizedBox(
+              height: MediaQuery.of(context).size.width * 0.9,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  initialCenter: _initialPosition,
+                  initialZoom: 16,
+                  onTap: (tapPosition, point) {
+                    _updateLocation(point);
+                  },
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    // subdomains: ['a', 'b', 'c'],
+                  ),
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: _initialPosition,
+                        width: 80,
+                        height: 80,
+                        child: const Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                          size: 40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            const LabeledEditTextfield(
+              label: 'Enter Your Location',
+              keyboardType: TextInputType.text,
+            ),
+            const SizedBox(height: 20),
+            MyThirdButton(
+              onTap: () {
+                _updateLocation(_initialPosition);
+              },
+              buttonText: 'Save',
+            ),
+          ],
+        ),
       ),
     );
   }
