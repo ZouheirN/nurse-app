2af487aded840c274902ed51c1298adade9ad969
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-09 17:18:38 +0300

    Update home_page.dart

--- lib/pages/user/home_page.dart
+++ lib/pages/user/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nurse_app/components/header.dart';
 import 'package:nurse_app/components/second_button.dart';
 
 class HomePage extends StatelessWidget {
@@ -13,50 +14,7 @@ class HomePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.location_on),
-                        onPressed: () {},
-                      ),
-                      const Text(
-                        'Beirut, Lebanon',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.notifications_none,
-                      color: Color(0xFF7BB442),
-                    ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/notification');
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.person),
-                    onPressed: () {},
-                  ),
-                  const Text(
-                    'Welcome Back User',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-              ),
+              const Header(),
               Padding(
                 padding: const EdgeInsets.all(16),
                 child: Column(
