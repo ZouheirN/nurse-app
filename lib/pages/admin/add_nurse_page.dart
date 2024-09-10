4be2915de2672ebb46d2bc30e7824627ce52a6d9
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-10 22:46:13 +0300

    Update add_nurse_page.dart

--- lib/pages/admin/add_nurse_page.dart
+++ lib/pages/admin/add_nurse_page.dart
@@ -1,12 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:nurse_app/consts.dart';
+import 'package:http/http.dart' as http;
 import 'package:nurse_app/components/admin_header.dart';
 import 'package:nurse_app/components/phone_field_admin.dart';
 import 'package:nurse_app/components/labeled_textfield_admin.dart';
 import 'package:nurse_app/components/third_button.dart';
 import 'package:nurse_app/components/pick_image.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class AddNursePage extends StatelessWidget {
+class AddNursePage extends StatefulWidget {
   const AddNursePage({super.key});
+
+  @override
+  State<AddNursePage> createState() => _AddNursePageState();
+}
+
+class _AddNursePageState extends State<AddNursePage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneNumberController = TextEditingController();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    phoneNumberController.dispose();
+    super.dispose();
+  }
+
+  void createNurse(String name, String phoneNumber, String email,
+      BuildContext context) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(KEY_ACCESS_TOKEN);
+
+      final response = await http.post(
+        Uri.parse('$HOST/admin/nurses'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+        body: {
+          'name': name,
+          'phone_number': phoneNumber,
+          'email': email,
+        },
+      );
+
+      if (response.statusCode == 201) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Nurse Created'),
+              content: const Text('Nurse created successfully.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Failed create nurse'),
+              content: const Text('Failed to create nurse. Please try again.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text(
+                'An error occurred. Please check your connection and try again later.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,15 +119,17 @@ class AddNursePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const SizedBox(height: 40),
-                  const LabeledTextfieldAdmin(
+                  LabeledTextfieldAdmin(
                     label: 'Nurse Name',
                     keyboardType: TextInputType.name,
+                    controller: nameController,
                   ),
                   const SizedBox(height: 10),
-                  const PhoneFieldAdmin(),
-                  const LabeledTextfieldAdmin(
+                  PhoneFieldAdmin(controller: phoneNumberController),
+                  LabeledTextfieldAdmin(
                     label: 'Nurse Address',
                     keyboardType: TextInputType.text,
+                    controller: emailController,
                   ),
                   const SizedBox(height: 20),
                   PickImage(
