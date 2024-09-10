6a5be28452ffaf8f9b1c388c83b91f1e6376cc5c
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-10 15:29:46 +0300

    Update labeled_edit_textfield.dart

--- lib/components/labeled_edit_textfield.dart
+++ lib/components/labeled_edit_textfield.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class LabeledEditTextfield extends StatefulWidget {
   final String label;
   final TextInputType keyboardType;
+  final TextEditingController? controller;
 
   const LabeledEditTextfield({
     super.key,
     required this.label,
     this.keyboardType = TextInputType.text,
+    this.controller,
   });
 
   @override
@@ -50,6 +52,7 @@ class _LabeledEditTextfieldState extends State<LabeledEditTextfield> {
             child: Focus(
               onFocusChange: _onFocusChange,
               child: TextField(
+                controller: widget.controller,
                 keyboardType: widget.keyboardType,
                 decoration: InputDecoration(
                   enabledBorder: const OutlineInputBorder(
@@ -60,8 +63,9 @@ class _LabeledEditTextfieldState extends State<LabeledEditTextfield> {
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                     borderSide: BorderSide(color: Color(0xFFC2C2C2)),
                   ),
-                  fillColor:
-                      _isFocused ? const Color.fromARGB(255, 185, 185, 185) : const Color(0xFFC2C2C2),
+                  fillColor: _isFocused
+                      ? const Color.fromARGB(255, 185, 185, 185)
+                      : const Color(0xFFC2C2C2),
                   filled: true,
                   contentPadding:
                       const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
