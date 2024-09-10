1d4ba2d67d40f3585189af337ef7aff556a18621
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-10 22:45:50 +0300

    Update labeled_textfield_admin.dart

--- lib/components/labeled_textfield_admin.dart
+++ lib/components/labeled_textfield_admin.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class LabeledTextfieldAdmin extends StatefulWidget {
   final String label;
   final TextInputType keyboardType;
+  final TextEditingController? controller;
 
   const LabeledTextfieldAdmin({
     super.key,
     required this.label,
     this.keyboardType = TextInputType.text,
+    this.controller,
   });
 
   @override
@@ -50,6 +52,7 @@ class _LabeledTextfieldAdminState extends State<LabeledTextfieldAdmin> {
             child: Focus(
               onFocusChange: _onFocusChange,
               child: TextField(
+                controller: widget.controller,
                 keyboardType: widget.keyboardType,
                 decoration: InputDecoration(
                   enabledBorder: const OutlineInputBorder(
