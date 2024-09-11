c67ab25c8beb3ccdc65e40dfa0d5792f54ae5060
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-11 21:19:08 +0300

    Create gender_selection_field_admin.dart

---
+++ lib/components/gender_selection_field_admin.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class GenderSelectionController {
+  String? selectedGender;
+
+  void selectGender(String gender) {
+    selectedGender = gender.toLowerCase();
+  }
+
+  String? getGender() {
+    return selectedGender;
+  }
+}
+
+class GenderSelectionFieldAdmin extends StatefulWidget {
+  final GenderSelectionController controller;
+
+  const GenderSelectionFieldAdmin({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  State<GenderSelectionFieldAdmin> createState() =>
+      _GenderSelectionFieldAdminState();
+}
+
+class _GenderSelectionFieldAdminState extends State<GenderSelectionFieldAdmin> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Select Nurse Gender',
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildGenderOption("Male"),
+              _buildGenderOption("Female"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _capitalize(String gender) {
+    return gender[0].toUpperCase() + gender.substring(1).toLowerCase();
+  }
+
+  Widget _buildGenderOption(String gender) {
+    bool isSelected = widget.controller.selectedGender == gender.toLowerCase();
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          widget.controller.selectGender(gender);
+        });
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.35,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        decoration: BoxDecoration(
+          color: isSelected
+              ? const Color.fromARGB(255, 245, 245, 245)
+              : const Color(0xFFE7E7E7),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: isSelected ? Colors.green : Colors.transparent,
+            width: 2,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            _capitalize(gender),
+            style: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
