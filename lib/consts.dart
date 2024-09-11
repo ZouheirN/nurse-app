91cb0b383796f3d6bcc1b6df6edc9c9c1a843051
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-11 16:27:54 +0300

    Update consts.dart

--- lib/consts.dart
+++ lib/consts.dart
@@ -2,4 +2,4 @@ const String HOST = "https://devzurapi.alahmadnursecare.com/api";
 
 const String KEY_ACCESS_TOKEN = "token";
 
-const String KEY_USER_EMAIL = "email";
+const String KEY_USER_NUMBER = "phoneNumber";
