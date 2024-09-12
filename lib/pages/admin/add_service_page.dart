42fa11c3ffb0125c8309e784a0f81f1e270eb1d3
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-12 22:04:12 +0300

    Update add_service_page.dart

--- lib/pages/admin/add_service_page.dart
+++ lib/pages/admin/add_service_page.dart
@@ -1,12 +1,115 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:nurse_app/components/admin_header.dart';
 import 'package:nurse_app/components/pick_image.dart';
 import 'package:nurse_app/components/third_button.dart';
 import 'package:nurse_app/components/labeled_textfield_admin.dart';
 import 'package:nurse_app/components/labeled_mini_textfield_admin.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:nurse_app/consts.dart';
 
-class AddServicePage extends StatelessWidget {
+class AddServicePage extends StatefulWidget {
   const AddServicePage({super.key});
+
+  @override
+  State<AddServicePage> createState() => _AddServicePageState();
+}
+
+class _AddServicePageState extends State<AddServicePage> {
+  final nameController = TextEditingController();
+  final priceController = TextEditingController();
+  final discountPriceController = TextEditingController();
+  String? selectedImage;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    priceController.dispose();
+    discountPriceController.dispose();
+    super.dispose();
+  }
+
+  void createService(String serviceName, int price, int discountPrice,
+      String? image, BuildContext context) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(KEY_ACCESS_TOKEN);
+
+      final response = await http.post(
+        Uri.parse('$HOST/admin/services'),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'name': serviceName,
+          'price': price,
+          'discount_price': discountPrice,
+          'image': image,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Service Created'),
+              content: const Text('Service created successfully.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      } else {
+        final errorData = json.decode(response.body);
+        final errorMessage = errorData['message'];
+
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Create Failed'),
+              content: Text(errorMessage),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+    } catch (e) {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Error'),
+            content: const Text('An error occurred. Please try again later.'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,25 +125,28 @@ class AddServicePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   const SizedBox(height: 40),
-                  const LabeledTextfieldAdmin(
+                  LabeledTextfieldAdmin(
                     label: 'Service Name',
                     keyboardType: TextInputType.name,
+                    controller: nameController,
                   ),
                   const SizedBox(height: 10),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 35),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 35),
                     child: Row(
                       children: [
                         Expanded(
                           child: LabeledMiniTextfieldAdmin(
                             label: 'Service Price',
                             keyboardType: TextInputType.number,
+                            controller: priceController,
                           ),
                         ),
                         Expanded(
                           child: LabeledMiniTextfieldAdmin(
                             label: 'Sale Price',
                             keyboardType: TextInputType.number,
+                            controller: discountPriceController,
                           ),
                         ),
                       ],
@@ -49,11 +155,26 @@ class AddServicePage extends StatelessWidget {
                   const SizedBox(height: 20),
                   PickImage(
                     label: 'Nurse Picture',
-                    onImageSelected: (image) {},
+                    onImageSelected: (image) {
+                      setState(() {
+                        selectedImage = image
+                            ?.path; // Adjust this according to how the image is handled
+                      });
+                    },
                   ),
                   const SizedBox(height: 20),
                   MyThirdButton(
-                    onTap: () {},
+                    onTap: () {
+                      final int price = int.tryParse(priceController.text) ?? 0;
+                      final int discountPrice = int.tryParse(discountPriceController.text) ?? 0;
+                      createService(
+                        nameController.text,
+                        price,
+                        discountPrice,
+                        selectedImage,
+                        context,
+                      );
+                    },
                     buttonText: 'Add Service',
                   ),
                 ],
