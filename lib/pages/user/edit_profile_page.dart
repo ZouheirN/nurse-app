6840e0493fed978c75e64f199d1f1d9be01bab7e
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-20 23:28:49 +0300

    Update edit_profile_page.dart

--- lib/pages/user/edit_profile_page.dart
+++ lib/pages/user/edit_profile_page.dart
@@ -3,8 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:nurse_app/consts.dart';
 import 'package:http/http.dart' as http;
-import 'package:nurse_app/components/third_button.dart';
 import 'package:quickalert/quickalert.dart';
+import 'package:nurse_app/components/loader.dart';
+import 'package:nurse_app/components/third_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:nurse_app/components/labeled_edit_textfield.dart';
 import 'package:nurse_app/components/edit_phone_number_field.dart';
@@ -18,6 +19,8 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   late int userId;
+  bool isLoading = true;
+
   String name = '';
   String phoneNumber = '';
   String email = '';
@@ -44,6 +47,10 @@ class _EditProfilePageState extends State<EditProfilePage> {
   }
 
   Future<void> fetchProfileData() async {
+    setState(() {
+      isLoading = true;
+    });
+
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString(KEY_ACCESS_TOKEN);
 
@@ -67,6 +74,8 @@ class _EditProfilePageState extends State<EditProfilePage> {
         phoneController.text = phoneNumber;
         emailController.text = email;
         locationController.text = location;
+
+        isLoading = false;
       });
     } else {
       print('Failed to load user data');
@@ -148,56 +157,61 @@ class _EditProfilePageState extends State<EditProfilePage> {
           ],
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFD9D9D9),
-                  border: Border.all(
-                    color: const Color(0xFFD9D9D9),
-                    width: 2,
+      body: SafeArea(
+        child: isLoading
+            ? const Loader()
+            : SingleChildScrollView(
+                physics: const AlwaysScrollableScrollPhysics(),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFD9D9D9),
+                          border: Border.all(
+                            color: const Color(0xFFD9D9D9),
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 10),
+                            LabeledEditTextfield(
+                              label: 'Username',
+                              keyboardType: TextInputType.name,
+                              controller: nameController,
+                            ),
+                            const SizedBox(height: 7),
+                            EditPhoneNumberField(controller: phoneController),
+                            LabeledEditTextfield(
+                              label: 'Email',
+                              keyboardType: TextInputType.emailAddress,
+                              controller: emailController,
+                            ),
+                            const SizedBox(height: 7),
+                            LabeledEditTextfield(
+                              label: 'Location',
+                              keyboardType: TextInputType.text,
+                              controller: locationController,
+                            ),
+                            const SizedBox(height: 20),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      MyThirdButton(
+                        onTap: () {
+                          updateProfile();
+                        },
+                        buttonText: 'Save',
+                      ),
+                    ],
                   ),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 10),
-                    LabeledEditTextfield(
-                      label: 'Username',
-                      keyboardType: TextInputType.name,
-                      controller: nameController,
-                    ),
-                    const SizedBox(height: 7),
-                    EditPhoneNumberField(controller: phoneController),
-                    LabeledEditTextfield(
-                      label: 'Email',
-                      keyboardType: TextInputType.emailAddress,
-                      controller: emailController,
-                    ),
-                    const SizedBox(height: 7),
-                    LabeledEditTextfield(
-                      label: 'Location',
-                      keyboardType: TextInputType.text,
-                      controller: locationController,
-                    ),
-                    const SizedBox(height: 20),
-                  ],
                 ),
               ),
-              const SizedBox(height: 20),
-              MyThirdButton(
-                onTap: () {
-                  updateProfile();
-                },
-                buttonText: 'Save',
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
