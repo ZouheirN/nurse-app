f6b15ca5ed01c17d8260fe9221032ebac2357a57
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-09-22 13:14:25 +0300

    Update manage_services_page.dart

--- lib/pages/admin/manage_services_page.dart
+++ lib/pages/admin/manage_services_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:nurse_app/components/loader.dart';
 import 'package:nurse_app/consts.dart';
 import 'package:http/http.dart' as http;
 import 'package:nurse_app/components/admin_card.dart';
@@ -26,6 +27,10 @@ class _ManageServicesPageState extends State<ManageServicesPage> {
   }
 
   Future<void> fetchServices() async {
+    setState(() {
+      isLoading = true;
+    });
+
     final prefs = await SharedPreferences.getInstance();
     final token = prefs.getString(KEY_ACCESS_TOKEN);
 
@@ -55,70 +60,81 @@ class _ManageServicesPageState extends State<ManageServicesPage> {
     }
   }
 
+  Future<void> _handleRefresh() async {
+    await fetchServices();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: const AdminHeader(title: 'Manage Services'),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 20),
-                  AdminCard(
-                    imagePath: 'assets/images/image-gallery.png',
-                    text: 'Add Service',
-                    onTap: () {
-                      Navigator.pushNamed(context, '/addService');
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        'Services',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.w700,
+      body: SafeArea(
+        child: isLoading
+            ? const Loader()
+            : RefreshIndicator(
+                onRefresh: _handleRefresh,
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const SizedBox(height: 20),
+                            AdminCard(
+                              imagePath: 'assets/images/image-gallery.png',
+                              text: 'Add Service',
+                              onTap: () {
+                                Navigator.pushNamed(context, '/addService');
+                              },
+                            ),
+                            const SizedBox(height: 20),
+                            const Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Align(
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Services',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Column(
+                              children: services.map((service) {
+                                return Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 5),
+                                  child: ServiceCardAdmin(
+                                    imagePath: service['service_pic'] ??
+                                        'assets/images/square_logo.png',
+                                    title: service['name'],
+                                    price: service['price'],
+                                    salePrice: service['discount_price'],
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                        context,
+                                        '/editService',
+                                        arguments: service['id'],
+                                      );
+                                    },
+                                  ),
+                                );
+                              }).toList(),
+                            ),
+                          ],
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                  const SizedBox(height: 10),
-                  isLoading
-                      ? const CircularProgressIndicator()
-                      : Column(
-                          children: services.map((service) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 5),
-                              child: ServiceCardAdmin(
-                                imagePath: service['service_pic'] ??
-                                    'assets/images/square_logo.png',
-                                title: service['name'],
-                                price: service['price'],
-                                salePrice: service['discount_price'],
-                                onTap: () {
-                                  Navigator.pushNamed(
-                                    context,
-                                    '/editService',
-                                    arguments: service['id'],
-                                  );
-                                },
-                              ),
-                            );
-                          }).toList(),
-                        ),
-                ],
+                ),
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
