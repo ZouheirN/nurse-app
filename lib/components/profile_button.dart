de67991ba9e26ac3bb8570f59a648fcc7e300bfe
Zouheir Nakouzi <zouheir2002@gmail.com>
2025-07-07 17:00:26 +0300

    profile changes

---
+++ lib/components/profile_button.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class ProfileButton extends StatelessWidget {
+  final ImageProvider iconImage;
+  final String buttonText;
+  final Function? onTap;
+
+  const ProfileButton(
+      {super.key,
+      required this.iconImage,
+      required this.buttonText,
+      this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (onTap != null) {
+          onTap!();
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: const LinearGradient(
+            colors: [
+              Color.fromRGBO(19, 27, 10, 1),
+              Color.fromRGBO(101, 148, 54, 1),
+            ],
+            begin: Alignment.centerRight,
+            end: Alignment.centerLeft,
+          ),
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image(
+                image: iconImage,
+                height: 30,
+                width: 30,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 8.0,
+              ),
+              child: Text(
+                buttonText,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/user/settings_page.dart
+++ lib/pages/user/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:nurse_app/components/logout_button.dart';
+import 'package:nurse_app/components/profile_button.dart';
 import 'package:nurse_app/components/settings_button.dart';
+import 'package:nurse_app/features/authentication/models/user_model.dart';
 
 import '../../services/user.dart';
 
@@ -9,8 +11,27 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    UserModel? user = UserBox.getUser();
+
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xFFF3FDE9),
+      appBar: AppBar(
+        title: Text(
+          user!.name.toString(),
+          style: const TextStyle(
+            fontSize: 36,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+        centerTitle: true,
+        flexibleSpace: const Image(
+          image: AssetImage('assets/images/header_background.png'),
+          fit: BoxFit.cover,
+        ),
+        backgroundColor: Colors.transparent,
+      ),
       body: Stack(
         children: [
           Column(
@@ -20,68 +41,256 @@ class SettingsPage extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(height: 40),
-                    const Center(
-                      child: Image(
-                        image: AssetImage('assets/images/logo.png'),
-                        height: 110,
-                        width: 220,
+                    // const SizedBox(height: 40),
+                    // const Center(
+                    //   child: Image(
+                    //     image: AssetImage('assets/images/logo.png'),
+                    //     height: 110,
+                    //     width: 220,
+                    //   ),
+                    // ),
+                    const SizedBox(height: 16),
+                    // Center(
+                    //   child: SettingsButton(
+                    //     icon: const Icon(Icons.account_circle),
+                    //     buttonText: 'Account Details',
+                    //     onTap: () {
+                    //       Navigator.pushNamed(context, '/editProfile');
+                    //     },
+                    //   ),
+                    // ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      margin: const EdgeInsets.symmetric(horizontal: 12),
+                      elevation: 0,
+                      color: const Color.fromRGBO(255, 255, 255, 1),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ProfileButton(
+                              iconImage: const AssetImage(
+                                'assets/images/user2.png',
+                              ),
+                              buttonText: 'Profile',
+                              onTap: () {
+                                Navigator.pushNamed(context, '/editProfile');
+                              },
+                            ),
+                            ProfileButton(
+                              iconImage: const AssetImage(
+                                'assets/images/support.png',
+                              ),
+                              buttonText: 'Support',
+                              onTap: () {
+                                Navigator.pushNamed(context, '/editProfile');
+                              },
+                            ),
+                            ProfileButton(
+                              iconImage: const AssetImage(
+                                'assets/images/language.png',
+                              ),
+                              buttonText: 'Language',
+                              onTap: () {
+                                Navigator.pushNamed(context, '/editProfile');
+                              },
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 40),
-                    Center(
-                      child: SettingsButton(
-                        icon: const Icon(Icons.account_circle),
-                        buttonText: 'Account Details',
-                        onTap: () {
-                          Navigator.pushNamed(context, '/editProfile');
-                        },
+                    const SizedBox(height: 12),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      margin: const EdgeInsets.symmetric(horizontal: 12),
+                      elevation: 0,
+                      color: const Color.fromRGBO(255, 255, 255, 1),
+                      child: Column(
+                        children: [
+                          const ListTile(
+                            dense: true,
+                            title: Text(
+                              'Account Details',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Image.asset(
+                              'assets/images/notification2.png',
+                              height: 25,
+                              width: 25,
+                            ),
+                            title: const Text(
+                              'Notifications',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: Divider(
+                              color: Color.fromRGBO(120, 120, 120, 1),
+                              height: 1,
+                            ),
+                          ),
+                          ListTile(
+                            onTap: () {
+                              Navigator.pushNamed(context, '/updateLocation');
+                            },
+                            leading: const Icon(Icons.location_on_outlined),
+                            title: const Text(
+                              'Location',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Center(
-                      child: SettingsButton(
-                        icon: const Icon(Icons.location_on),
-                        buttonText: 'Update Location',
-                        onTap: () {
-                          Navigator.pushNamed(context, '/updateLocation');
-                        },
+                    // Center(
+                    //   child: SettingsButton(
+                    //     icon: const Icon(Icons.location_on),
+                    //     buttonText: 'Update Location',
+                    //     onTap: () {
+                    //       Navigator.pushNamed(context, '/updateLocation');
+                    //     },
+                    //   ),
+                    // ),
+                    const SizedBox(height: 14),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      margin: const EdgeInsets.symmetric(horizontal: 12),
+                      elevation: 0,
+                      color: const Color.fromRGBO(255, 255, 255, 1),
+                      child: Column(
+                        children: [
+                          const ListTile(
+                            dense: true,
+                            title: Text(
+                              'Help Center',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Image.asset(
+                              'assets/images/job.png',
+                              height: 25,
+                              width: 25,
+                            ),
+                            title: const Text(
+                              'Apply for a Job',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            trailing: const Icon(
+                              Icons.chevron_right,
+                              size: 20,
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 16,
+                            ),
+                            child: Divider(
+                              color: Color.fromRGBO(120, 120, 120, 1),
+                              height: 1,
+                            ),
+                          ),
+                          ExpansionTile(
+                            iconColor: Colors.black,
+                            collapsedIconColor: Colors.black,
+                            leading: Image.asset(
+                              'assets/images/faq.png',
+                              height: 25,
+                              width: 25,
+                            ),
+                            title: const Text(
+                              'FAQs',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            collapsedShape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 24),
-                      child: LogoutButton(
-                        icon: const Icon(Icons.logout),
-                        buttonText: 'Logout',
-                        onTap: () {
-                          logoutUser();
-                          Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
-                        },
-                      ),
-                    ),
+                    // Padding(
+                    //   padding: const EdgeInsets.symmetric(horizontal: 24),
+                    //   child: LogoutButton(
+                    //     icon: const Icon(Icons.logout),
+                    //     buttonText: 'Logout',
+                    //     onTap: () {
+                    //       logoutUser();
+                    //       Navigator.pushNamedAndRemoveUntil(
+                    //           context, '/login', (route) => false);
+                    //     },
+                    //   ),
+                    // ),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 30),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 16),
                 child: Center(
-                  child: Column(
-                    children: [
-                      const Text(
-                        'Powered By',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w900),
+                  child: ColorFiltered(
+                    colorFilter:
+                        ColorFilter.mode(Colors.black, BlendMode.srcATop),
+                    child: Image(
+                      height: 80,
+                      image: AssetImage(
+                        'assets/images/square_logo.png',
                       ),
-                      const SizedBox(height: 3),
-                      Image.asset(
-                        'assets/images/powered_by.png',
-                        width: 150,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
+              // Padding(
+              //   padding: const EdgeInsets.only(bottom: 30),
+              //   child: Center(
+              //     child: Column(
+              //       children: [
+              //         const Text(
+              //           'Powered By',
+              //           style: TextStyle(
+              //               fontSize: 18, fontWeight: FontWeight.w900),
+              //         ),
+              //         const SizedBox(height: 3),
+              //         Image.asset(
+              //           'assets/images/powered_by.png',
+              //           width: 150,
+              //         ),
+              //       ],
+              //     ),
+              //   ),
+              // ),
             ],
           ),
         ],
